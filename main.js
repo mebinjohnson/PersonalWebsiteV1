8447aa87f51ff6486a3309faa50241164f5ba69b
mebinjohnson <mebinjohnson@gmail.com>
2018-07-11 21:06:05 +0400

    Git Ini commit

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-    var currentScrollpos = window.pageYOffset;
-    if(prevScrollpos > currentScrollpos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-100px";
-        }
-
-    prevScrollpos = currentScrollpos;
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
 }
+
+
+$("#toggle").click(function() {
+  
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+
+    });
