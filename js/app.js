be8581b038fa4cd086786aa477a21740da1c637e
mebinjohnson <mebinjohnson@gmail.com>
2018-07-19 02:07:36 +0400

    Minify

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).foundation()
 if (document.documentElement.clientWidth < 768) {
-    var prev = 0;
-    var $window = $(window);
-    var nav = $('.navbar');
+    // var prev = 0;
+    // var $window = $(window);
+    // var nav = $('.navbar');
 
-    $window.on('scroll', function () {
-        var scrollTop = $window.scrollTop();
-        nav.toggleClass('hidden', scrollTop > prev);
-        prev = scrollTop;
-    });
+    // $window.on('scroll', function () {
+    //     var scrollTop = $window.scrollTop();
+    //     nav.toggleClass('hidden', scrollTop > prev);
+    //     prev = scrollTop;
+    // });
 }
 else {
     $(window).scroll(function (e) {
